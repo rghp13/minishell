b3ce65ac91e6456816bf19999286839266b1fc27
Dimitri Scriabine <scriabine.dimitri@gmail.com>
2021-12-10 16:30:32 +0100

    kinda trying here

--- srcs/function_parsing.c
+++ srcs/function_parsing.c
@@ -11,6 +11,49 @@ int	is_separator(char c, char cnext)
 	return (0);
 }
 
+int	is_redirector(char c, char next)
+{
+	if (c == '>' && next == '>')
+		return (4);
+	if (c == '<' && next == '<')
+		return (3);
+	if (c == '>')
+		return (2);
+	if (c == '<')
+		return (1);
+	return (0);
+}
+
+int	parse_command(t_command	*command, int start, int end, char *line)
+{
+	int	i;
+	int	j;
+
+	i = start;
+	while (line[i] && !is_redirector(line[i], line [i + 1]))
+		i++;
+	if (small_malloc(&command->command, sizeof(char) * ((i - start) + 1)))
+		return (-1);
+	ft_memcpy(command->command, &line[start], i - start);
+	command->command[(i - start) - 1] = '\0';
+	command->redirector = is_redirector(line[i], line[i + 1]);
+	command->file = NULL;
+	command->separator = is_separator(line[end], line[end + 1]);
+	if (command->redirector == 0)
+		return (0);
+	i += 1 + (is_redirector(line[i], line [i + 1]) > 2);
+	j = i;
+	while (line[i] && !is_separator(line[i], line[i + 1]))
+		i++;
+	if (small_malloc(&command->file, sizeof(char) * ((i - j) + 1)))
+		return (-1);
+	ft_memcpy(command->file, &line[j], i - j);
+	command->file[(i - j) - 1] = '\0';
+	if (small_malloc(&command->next, sizeof(t_command) * 1))
+		return (-1);
+	return (0);
+}
+
 t_command	*function_parse(char *line)
 {
 	int			i;
@@ -29,11 +72,14 @@ t_command	*function_parse(char *line)
 	{
 		if (is_separator(line[i], line[i + 1]))
 		{
-			/*do the thing*/
+			parse_command(current, j, i, line);
+			current->separator = is_separator(line[i], line[i + 1]);
 			j = i;
 			i += (is_separator(line[i], line[i + 1] > 1));
 		}
 		i++;
 	}
+	free(current->next);
+	current->next = NULL;
 	return (first);
 }
