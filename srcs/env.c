4ca914f85580986a04f5c1f327990f520a720cb5
Romain PONSONNET <rponsonn@e1r6p18.42.fr>
2022-01-05 18:49:14 +0100

    fixed bugs on env generation, added function to get value of a key

--- srcs/env.c
+++ srcs/env.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-t_cont	*get_env(char **envp)
+t_env	*get_env(char **envp)
 {
 	int		i;
 	t_env	*head;
@@ -9,26 +9,36 @@ t_cont	*get_env(char **envp)
 	i = 0;
 	head = NULL;
 	if (envp == NULL || envp[0] == NULL)
-		return (ft_error(1));
+	{
+		return (NULL);
+	}
 	while (envp[i])
 	{
 		current = ft_calloc(1, sizeof(t_env));
-		if (current == NULL)
-			return(ft_free_envp(&current, &head));
-		if (assign_env_to_struct(&current, envp[i]) == 1)
-			return (ft_free_envp(&current, &head));
-		add_last(&head, &current);
+		if (current == NULL || assign_env_to_struct(current, envp[i]) == 1)
+		{
+			free_envp(current, head);
+			return (NULL);
+		}
+		if (head == NULL)
+			head = current;
+		else
+			add_last(head, current);
 		i++;
 	}
 	return (head);
 }
 
-void	add_last(t_env *head, t_env *current)//need to test this
+/*
+**doesn't add last if head empty (no thoughts)
+*/
+
+void	add_last(t_env *head, t_env *current)
 {
 	t_env *next;
 
-	if (head == NULL)
-		*head = current;
+	if (head == NULL || current == NULL)
+		return ;
 	else
 	{
 		next = head;
@@ -37,33 +47,6 @@ void	add_last(t_env *head, t_env *current)//need to test this
 		next->next = current;
 		current->prev = next;
 	}
-}
-
-int	ft_free_envp(t_env *current, t_env *head)
-{
-	t_env *ptr;
-
-	if (current != NULL)
-	{
-		if (current->key != NULL)
-			free(current->key);
-		if (current->value != NULL)
-			free(current->value);
-		free(current);
-	}
-	current = NULL;
-	while (head != NULL)
-	{
-		ptr = head->next;
-		if (head->key != NULL)
-			free(head->key);
-		if (head->value != NULL)
-			free(head->value);
-		free(head);
-		head = ptr;
-	}
-	head = NULL;
-	return (1);
 }
 
 int	assign_env_to_struct(t_env *current, char *envp)
@@ -83,4 +66,39 @@ int	assign_env_to_struct(t_env *current, char *envp)
 	if (current->value == NULL)
 		return (1);
 	return (0);
+}
+
+char	*get_key_val(const char *key, t_env *env)
+{
+	const char *ret;
+	t_env *key_struct;
+
+	if (key[0] != '$')
+		return (ft_calloc(1, sizeof(char)));
+	ret = key + 1;
+	key_struct = find_env(ret, env);
+	if (key_struct == NULL)
+		return (ft_calloc(1, sizeof(char)));
+	return (ft_strdup(key_struct->value));
+}
+
+t_env	*find_env(const char *key, t_env *head)
+{
+	t_env	*ptr;
+	int		len;
+
+	ptr = head;
+	if (key == NULL || head == NULL)
+		return (NULL);
+	len = ft_strlen(key);
+	while (ptr)
+	{
+		if (len == ft_strlen(ptr->key))
+		{
+			if (ft_strncmp(key, ptr->key, len) == 0)
+				return (ptr);
+		}
+		ptr = ptr->next;
+	}
+	return (NULL);
 }
