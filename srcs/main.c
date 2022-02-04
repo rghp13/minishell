6bed471c8ab38e3ebbac1366e336ce9340402b69
Romain <rghp13@gmail.com>
2022-02-04 18:18:25 +0100

    touching up main then merging

--- srcs/main.c
+++ srcs/main.c
@@ -1,16 +1,6 @@
 #include "../includes/minishell.h"
 
 /*
-**readline, rl_clear_history, rl_on_new_line,
-**rl_replace_line, rl_redisplay, add_history,
-**printf, malloc, free, write, access, open, read,
-**close, fork, wait, waitpid, wait3, wait4, signal,
-**sigaction, kill, exit, getcwd, chdir, stat,
-**lstat, fstat, unlink, execve, dup, dup2, pipe,
-**opendir, readdir, closedir, strerror, perror,
-**isatty, ttyname, ttyslot, ioctl, getenv, tcsetattr,
-**tcgetattr, tgetent, tgetflag, tgetnum, tgetstr,
-**tgoto, tputs
 **ERRORS NEED TO GO TO STDERR, TRACK SHELL LVL, IMPLEMENT HISTORY, 
 **'' not disappearing in echo, -n flag being printed
 **bash should say "home not set"
@@ -18,6 +8,7 @@
 **shell should error out if no environment
 **typing a command then pressing CTRL+D twice executes it (shouldn't happen)
 **implement ~/ execution
+**test what happens with ~/ if it's after a pipe
 **"hi.txt" ctrl+d
 */
 
