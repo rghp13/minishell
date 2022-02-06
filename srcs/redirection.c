24825c9f958faeb85e7c32ce54521a4a707472ee
Dimitri Scriabine <dimitriscr@pop-os.localdomain>
2022-02-06 16:24:07 +0100

    fixed filename spaces breaking open and files being created without proper permissions

--- srcs/redirection.c
+++ srcs/redirection.c
@@ -5,9 +5,9 @@ int	prepare_redirection(t_cmd *cmd, t_cont *cont)
 	if (cmd->input_type > -1)
 	{
 		if (cmd->input_type == 0)
-			cont->fd_in = open(cmd->input, O_RDONLY);
+			cont->fd_in = open(cmd->input, O_RDONLY, 0664);
 		else if (cmd->input_type == 1)
-			cont->fd_in = open(cmd->input, O_RDONLY); //This needs to be changed for <<
+			cont->fd_in = open(cmd->input, O_RDONLY, 0664); //This needs to be changed for <<
 		if (cont->fd_in == -1)
 			return (-1);
 		dup2(cont->fd_in, 0);
@@ -16,9 +16,9 @@ int	prepare_redirection(t_cmd *cmd, t_cont *cont)
 	if (cmd->output_type > -1)
 	{
 		if (cmd->output_type == 0)
-			cont->fd_out = open(cmd->output, O_WRONLY | O_TRUNC | O_CREAT);
+			cont->fd_out = open(cmd->output, O_WRONLY | O_TRUNC | O_CREAT, 0664);
 		else if (cmd->output_type == 1)
-			cont->fd_out = open(cmd->output, O_WRONLY | O_APPEND | O_CREAT);
+			cont->fd_out = open(cmd->output, O_WRONLY | O_APPEND | O_CREAT, 0664);
 		if (cont->fd_out == -1)
 			return (-2);
 		dup2(cont->fd_out, 1);
