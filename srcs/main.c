d0db2e267313c79a9d780e2995242973f58f806f
Dimitri Scriabine <dimitriscr@pop-os.localdomain>
2022-02-08 19:03:37 +0100

    started double left arrow

--- srcs/main.c
+++ srcs/main.c
@@ -18,7 +18,7 @@ int	initialize_main_struct(t_cont *cont, char **envp, struct termios *original)
 
 	tcgetattr(0, original);
 	t = *original;
-	t.c_lflag &= (ICANON | ~ECHOCTL); // | NOFLSH | ~ECHOK
+	t.c_lflag &= (ICANON | ~ECHOCTL);
 	t.c_cc[VQUIT] = 0;
 	tcsetattr(0, TCSANOW, &t);
 	signal_redirector(cont, 0, 1);
