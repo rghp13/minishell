519110fcec854b242d4dd6db8344101208de5c1f
Romain Ponsonnet <rghp13@gmail.com>
2022-02-17 03:50:19 +0100

    removed unused function and troubleshooting text

--- srcs/executils.c
+++ srcs/executils.c
@@ -3,33 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   executils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dimitriscr <dimitriscr@student.42.fr>      +#+  +:+       +#+        */
+/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 02:59:10 by dimitriscr        #+#    #+#             */
-/*   Updated: 2022/02/17 03:00:51 by dimitriscr       ###   ########.fr       */
+/*   Updated: 2022/02/17 03:44:17 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-//possibly removing this function
-char	*get_home_path(const char *str, t_cont *cont)
-{
-	char	*ptr;
-	char	*hold;
-
-	ptr = get_key_val("$HOME", cont->env);
-	if (!ptr)
-	{
-		ft_putstr_fd("bash: HOME not set\n", STDERR_FILENO);
-		return (NULL);
-	}
-	str++;
-	hold = ft_strjoin(ptr, str);
-	free(ptr);
-	printf("%s\n", hold);
-	return (hold);
-}
 
 int	relative_path_bridge(t_cmd *cmd, t_env *env)
 {
@@ -47,8 +28,6 @@ int	relative_path_bridge(t_cmd *cmd, t_env *env)
 
 int	err_ret_value(int erno, t_cmd *cmd)
 {
-	ft_putnbr_fd(erno, STDERR_FILENO);
-	ft_putchar_fd('\n', STDERR_FILENO);
 	if (cmd->abspath == NULL && access(cmd->arg[0], F_OK) == 0)
 	{
 		if (access(cmd->arg[0], X_OK))
